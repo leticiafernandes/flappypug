535434d74963cb25b2bba1cf110f8e3c51753ded
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-03-24 12:26:12 -0300

    renaming variable do pipeWidth and addinf offscreen function

--- libraries/pipe.js
+++ libraries/pipe.js
@@ -2,17 +2,21 @@ function Pipe(){
   this.top = random(height/2); // random quantity between top and middle of the screen
   this.bottom = random(height/2);
   this.vertical = width;
-  this.w = 20;
-  this.speed = 5;
+  this.pipeWidth = 25;
+  this.speed = 2;
 
   this.show = function() {
     fill(255);
-    rect(this.vertical, 0, this.w, this.top); // 0 starts from top
-    rect(this.vertical, height-this.bottom, this.w, this.bottom);
+    rect(this.vertical, 0, this.pipeWidth, this.top); // 0 starts from top
+    rect(this.vertical, height-this.bottom, this.pipeWidth, this.bottom);
   }
 
   this.update = function() {
     this.vertical -= this.speed;
+  }
+
+  this.offscreen = function() {
+    return this.vertical < -this.pipeWidth; // if vertical minor than pipe width
   }
 
 }
