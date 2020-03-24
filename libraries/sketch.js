0c41af5698f3f22280a91f169e1f4a529a01ce12
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-03-24 19:09:45 -0300

    including hit function

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -9,21 +9,25 @@ function setup() {
 
 function draw() {
   background(0);
-  pug.update();
-  pug.show();
 
-  if(frameCount % 100 == 0) { // a cada 100 frames coloca um pipe
-    pipes.push(new Pipe());
-  }
-
-  for(var i=0; i < pipes.length; i++){
+  for (var i = pipes.length - 1; i >= 0; i--) {
     pipes[i].show();
     pipes[i].update();
 
-    if(pipes[i].offscreen()) {
-      pipes.splice(i, 1); // delete pipe[i] from array pipes
-      i--;
+    if (pipes[i].hits(pug)) {
+      console.log('ouch');
     }
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1); // delete pipes that got of the
+    }
+  }
+
+  pug.update();
+  pug.show();
+
+  if (frameCount % 100 == 0) { // a cada 100 frames coloca um pipe
+    pipes.push(new Pipe());
   }
 }
 
