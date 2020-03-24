a2b03c4583b5d7f0c30a5b9bbe99e6e503f3d94e
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-03-24 19:10:02 -0300

    hits function

--- libraries/pipe.js
+++ libraries/pipe.js
@@ -1,22 +1,47 @@
 function Pipe(){
   this.top = random(height/2); // random quantity between top and middle of the screen
   this.bottom = random(height/2);
-  this.vertical = width;
-  this.pipeWidth = 25;
+  this.x = width;
+  this.pipeWidth = 20;
   this.speed = 2;
+
+  this.highlight = false;
 
   this.show = function() {
     fill(255);
-    rect(this.vertical, 0, this.pipeWidth, this.top); // 0 starts from top
-    rect(this.vertical, height-this.bottom, this.pipeWidth, this.bottom);
+    
+    if(this.highlight) {
+      fill(2555,0,0);
+    }
+    
+    rect(this.x, 0, this.pipeWidth, this.top); // 0 starts from top
+    rect(this.x, height-this.bottom, this.pipeWidth, this.bottom);
   }
 
   this.update = function() {
-    this.vertical -= this.speed;
+    this.x -= this.speed;
+  }
+
+  this.hits = function(pug) {
+    let isInferiorThanPipeTop = pug.y < this.top;
+    let isSuperiorThanPipeBottom = pug.y > height - this.bottom;
+
+    // check if between the x position of the pipe
+    let isXSuperior = pug.x > this.x;
+    let isXInferiorPlusPipeWidth = pug.x < this.x + this.pipeWidth;
+
+    if(isInferiorThanPipeTop || isSuperiorThanPipeBottom) {
+      if(isXSuperior && isXInferiorPlusPipeWidth) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
   }
 
   this.offscreen = function() {
-    return this.vertical < -this.pipeWidth; // if vertical minor than pipe width
+    return this.x < -this.pipeWidth; // if vertical minor than pipe width
   }
 
 }
