a32b0ced226879754517f7a9c295feb65b9a04a9
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-03-24 12:27:18 -0300

    adding pipes to each 100 frames

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -12,9 +12,18 @@ function draw() {
   pug.update();
   pug.show();
 
+  if(frameCount % 100 == 0) { // a cada 100 frames coloca um pipe
+    pipes.push(new Pipe());
+  }
+
   for(var i=0; i < pipes.length; i++){
     pipes[i].show();
     pipes[i].update();
+
+    if(pipes[i].offscreen()) {
+      pipes.splice(i, 1); // delete pipe[i] from array pipes
+      i--;
+    }
   }
 }
 
