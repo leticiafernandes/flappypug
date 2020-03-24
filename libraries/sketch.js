c863e4bed692dae3a4884bebc10a035d85f9356a
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-03-23 21:27:09 -0300

    game canvas

---
+++ libraries/sketch.js
@@ -0,0 +1,18 @@
+let bird;
+
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird();
+}
+
+function draw() {
+  background(0);
+  bird.update();
+  bird.show();
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+  }
+}
