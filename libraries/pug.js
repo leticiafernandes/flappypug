e1d15c947564d842d09c5b94bba59bbb3cad95a4
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-03-24 19:10:26 -0300

    adjusting velocity and air resitance

--- libraries/pug.js
+++ libraries/pug.js
@@ -1,14 +1,14 @@
 function Pug() {
-  this.vertical = height/2;
-  this.horizontal = 25;
+  this.y = height/2;
+  this.x = 25;
 
-  this.gravity = 1;
-  this.lift = -15; // how strong is lift level
+  this.gravity = 0.6;
+  this.lift = -15 ; // how strong is lift level
   this.velocity = 0;
 
   this.show = function () {
     fill(255);
-    ellipse(this.horizontal, this.vertical, 20, 20);
+    ellipse(this.x, this.y, 32, 32);
   }
 
   this.up = function () {
@@ -17,15 +17,16 @@ function Pug() {
 
   this.update = function() {
     this.velocity += this.gravity;
-    this.vertical += this.velocity; 
+    this.velocity *= 0.9; // air resistance
+    this.y += this.velocity; 
 
-    if(this.vertical > height){
-      this.vertical = height;
+    if(this.y > height){
+      this.y = height;
       this.velocity = 0;
     }
 
-    if(this.vertical < 0){
-      this.vertical = 0;
+    if(this.y < 0){
+      this.y = 0;
       this.velocity = 0;
     }
   }
